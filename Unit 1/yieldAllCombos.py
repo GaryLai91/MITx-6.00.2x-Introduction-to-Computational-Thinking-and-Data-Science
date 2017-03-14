2ab6cc9dad0c4f9bfd81a767698dc291975017b1
GaryLai91 <garylai91@gmail.com>
2017-03-14 05:54:48 -0500

    Add files via upload

---
+++ Unit 1/yieldAllCombos.py
@@ -0,0 +1,56 @@
+import random
+
+def yieldAllCombos(items):
+    N = len(items)
+    # enumerate the 2**N possible combinations
+    for i in range(3**N):
+        bag1 = []
+        bag2 = []
+        for j in range(N):
+            # test bit jth of integer i
+            x = (i // (3 ** j)) % 3
+            if x == 1:
+                bag1.append(items[j])
+            elif x == 2:
+                bag2.append(items[j])
+        yield (bag1, bag2)
+        
+
+class Item(object):
+    def __init__(self, n, v, w):
+        self.name = n
+        self.value = float(v)
+        self.weight = float(w)
+    def getName(self):
+        return self.name
+    def getValue(self):
+        return self.value
+    def getWeight(self):
+        return self.weight
+    def __str__(self):
+        return '<' + self.name + ', ' + str(self.value) + ', '\
+                     + str(self.weight) + '>'
+
+def buildRandomItems(n):
+    return [Item(str(i),10*random.randint(1,10),random.randint(1,10))
+            for i in range(n)]
+          
+          
+def buildItems():
+    return [Item(n,v,w) for n,v,w in (('clock', 175, 10),
+                                      ('painting', 90, 9),
+                                      ('radio', 20, 4),
+                                      ('vase', 50, 2),
+                                      ('book', 10, 1),
+                                      ('computer', 200, 20))]
+
+def powerSet(items):
+    N = len(items)
+    # enumerate the 2**N possible combinations
+    for i in range(2**N):
+        combo = []
+        for j in range(N):
+            # test bit jth of integer i
+            if (i >> j) % 2 == 1:
+                combo.append(items[j])
+        yield combo
